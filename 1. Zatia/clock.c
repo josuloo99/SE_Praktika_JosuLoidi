52db70526eb2d6f8460eb2d8dca125144cc521b6
Josu <j@gmail.com>
2020-10-08 12:49:48 +0200

    Lehen commit

---
+++ 1. Zatia/clock.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <semaphore.h> 
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
